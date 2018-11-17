9973c5584bd0adafea3cc92985a2317e801a6d3e
Andres <torti.max@gmail.com>
2018-11-17 00:58:49 -0300

    Más codigo de inicialización para el CanSat

--- Firmware/RIOT/cansat/README.md
+++ Firmware/RIOT/cansat/README.md
@@ -2,63 +2,18 @@ CanSat
 ============
 
 Firmware para el microcontrolador supervisor que se encuentra dentro del CanSat. Para poder compilar el firmware primero es necesario
- configurar el ambiente de desarrollo. El siguiente setup fue testeado en Windows 10 64-bit pero debería funcionar también en otras versiones
- de Windows.
-  
+ configurar el ambiente de desarrollo. Para ellos necesitamos utilizar cualquier distribuciÃ³n de Linux, en nuestro caso se utilizÃ³
+ Ubuntu 18.04. Compilar en Windows puede ser posible a travÃ©s de MinGW o WSL de Windows 10, sin embargo, MinGW trajo algunos problemas y
+ WSL no fue testeado.
 
 **1 - Instalar ARM GCC Toolchain**
-   - Bajar e instalar el toolchain desde [here](https://developer.arm.com/open-source/gnu-toolchain/gnu-rm/downloads).  
-   - Para la carpeta de instalación elegir una ruta de destino **corta** y **sin espacios** como por ejemplo "C:/armgcc":
 
-     ![ARM GCC Path](http://i60.tinypic.com/2l9kvhz.jpg)
+Para instalar el toolchain simplemente ejecutamos `sudo apt-get install gcc-arm-none-eabi`
 
-   - Seleccionar la opción "Add path to environment variable", deseleccionar todas las demás opciones:
-   
-   ![Add to Environment Paths](http://i61.tinypic.com/xqfh9t.jpg)
+**2 - Instalar make**
 
-**2 - Instalar MinGW and MSYS**
-   - Descargar MINGW desde [here](https://sourceforge.net/projects/mingw/files/latest/download) e iniciar la instalación. Para la carpeta de instalación elegir una ruta de destino **corta** y **sin espacios** como por ejemplo "C:\MinGW":
-
-   ![MinGW Path](http://i61.tinypic.com/1zbs27a.jpg)
-
-   - Seleccionar los siguientes elementos para instalar: **mingw32-base**, **mingw32-gcc-g++**, **msys-base**
-
-   ![MINGW Items](http://i62.tinypic.com/b8oimp.jpg)
-
-   - Instalar también el terminal Mintty que se encuentra dentro de "All Packages": **msys-mintty**
-
-   ![MINTTY](http://i59.tinypic.com/152nsee.jpg)
-
-   - Hacer click en "**Apply Changes**" dentro del menú "**Installation**". 
-
-   - Ir a la ruta de instalación de MinGW y buscar el archivo "mintty.exe" que debiera estar en "**C:\MinGW\msys\1.0\bin**" y crear un acceso directo al mismo.
-    
-   - Hacer click derecho e ir a "Propiedades" del acceso directo y cambiar el "Destino" a: "**C:\MinGW\msys\1.0\bin\mintty.exe /bin/bash -l**"
-
-   ![MINTTY Target](http://i57.tinypic.com/3097p6d.jpg)
-
-   - Abrir la consola MINTTY con el acceso directo creado y ejecutar el siguiente comando en ella: **mingw-get update**
-
-   ![MINGW Updates](http://i62.tinypic.com/x56qtj.jpg)
-
-  - Ejecutar luego lo siguientes comandos uno por uno "**mingw-get install**" command: **msys-wget**, **msys-zlib**, **msys-unzip**, **msys-mktemp**
-
-   ![MINGW Tools](http://i61.tinypic.com/dnewdw.jpg)
-
-  - Agregar las siguientes rutas a las "Variables de Entorno" del usuario: "**C:\MinGW\bin**", "**C:\MinGW\msys\1.0\bin**"
-
-   ![Environment Variables](http://i62.tinypic.com/34xiyie.jpg)
-
-**4 - Instalar Python 3**
-
-  - Descargar la última versión de **Python 3** desde [aquí](https://www.python.org/downloads/).
-  
-  - Una vez instalado Python 3 ir al directorio de instalación que suele ser "C:\Users\{usuario}\AppData\Local\Programs\Python\Python37-32".
-  
-  - Debemos crear un symlink para que el comando "python3" sea reconocido como una llamada a "python.exe". Para ello abrimos una consola y nos dirigimos a "C:\Users\{usuario}\AppData\Local\Programs\Python\Python37-32".
-  
-  - Ejecutar el siguiente comando para crear el symlink: **mklink "python3.exe" "python.exe"**
+Ejecutamos `sudo apt-get install make`
 
 **3 - Listo!**
 
-Para más información puede consultarse la fuente original de esta guía en [la documentación de RIOT OS](https://github.com/RIOT-OS/RIOT/wiki/Build-RIOT-on-Windows-OS).
+Ya podemos compilar RIOT OS! En este caso se utilizÃ³ Visual Studio Code con el plugin `RIOT` como entorno de desarrollo.
--- Firmware/RIOT/cansat/config/cansat.c
+++ Firmware/RIOT/cansat/config/cansat.c
@@ -4,10 +4,63 @@
  *  Created on: Nov 12, 2018
  *      Author: tmax4
  */
+#include <stdint.h>
 #include "cansat.h"
 #include "stm32f3xx_hal.h"
 #include "adc.h"
 #include "stmclk.h"
+#include "periph/gpio.h"
+#include "periph/uart.h"
+#include "periph/i2c.h"
+
+// GPS UART driver callback
+static void gps_uart_rx_interrupt(void *arg, uint8_t data)
+{
+	(void)arg;
+	(void)data;
+}
+
+// Command UART driver callback
+static void command_uart_rx_interrupt(void *arg, uint8_t data)
+{
+	(void)arg;
+	(void)data;
+}
+
+/**
+ * This function initializes all the GPIO. Peripheral specific pins are initialized on the respective
+ *  peripheral function
+ */
+static void init_gpio(void)
+{
+	// Array with the following structure: PORT, PIN, MODE
+	unsigned int port_pin_mode[] = {
+		ENABLE_5V_PORT, ENABLE_5V_PIN, ENABLE_5V_MODE,
+		ENABLE_3V3_PORT, ENABLE_3V3_PIN, ENABLE_3V3_MODE,
+		CHG_ALLOWED_PORT, CHG_ALLOWED_PIN, CHG_ALLOWED_MODE,
+		CHG_DETECT_PORT, CHG_DETECT_PIN, CHG_DETECT_MODE,
+		SW_OPEN_PORT, SW_OPEN_PIN, SW_OPEN_MODE,
+		GPOUT_PORT, GPOUT_PIN, GPOUT_MODE,
+		P1_PORT, P1_PIN, P1_MODE,
+		CHARGING_PORT, CHARGING_PIN, CHARGING_MODE,
+		ISET_PORT, ISET_PIN, ISET_MODE,
+		IO0_PORT, IO0_PIN, IO0_MODE,
+		IO1_PORT, IO1_PIN, IO1_MODE,
+		IO2_PORT, IO2_PIN, IO2_MODE,
+		IO3_PORT, IO3_PIN, IO3_MODE,
+		CHG_ENABLE_PORT, CHG_ENABLE_PIN, CHG_ENABLE_MODE,
+		S_GLOBO_PORT, S_GLOBO_PIN, S_GLOBO_MODE,
+		S_PARACAIDAS_PORT, S_PARACAIDAS_PIN, S_PARACAIDAS_MODE,
+		I2C_ENABLE_PORT, I2C_ENABLE_PIN, I2C_ENABLE_MODE,
+		SPI_BUFFER_ENABLE_PORT, SPI_BUFFER_ENABLE_PIN, SPI_BUFFER_ENABLE_MODE,
+		IO_ENABLE_PORT, IO_ENABLE_PIN, IO_ENABLE_MODE};
+
+	// Initialize each pin
+	for (unsigned int n = 0; n < (sizeof(port_pin_mode) / sizeof(int)) - 3; n += 3)
+	{
+		gpio_init(GPIO_PIN(port_pin_mode[n], port_pin_mode[n + 1]), port_pin_mode[n + 2]);
+	}
+}
 
 void cansat_init(void)
 {
@@ -16,67 +69,16 @@ void cansat_init(void)
 	// THIS DOES NOT CONFIGURE THE USB CLOCK.
 	// See: https://riot-os.org/api/group__cpu__stm32__common.html
 	stmclk_init_sysclk();
+
+	// Configure GPIO pins
+	init_gpio();
+
+	// Init ADC1 and ADC2
 	MX_ADC1_Init();
 	MX_ADC2_Init();
 
-	RCC_OscInitTypeDef RCC_OscInitStruct;
-	RCC_ClkInitTypeDef RCC_ClkInitStruct;
-	RCC_PeriphCLKInitTypeDef PeriphClkInit;
-
-	// Configure LSE Drive Capability
-	HAL_PWR_EnableBkUpAccess();
-
-	__HAL_RCC_LSEDRIVE_CONFIG(RCC_LSEDRIVE_LOW);
-
-	// Initializes the CPU, AHB and APB busses clocks
-	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI|RCC_OSCILLATORTYPE_HSE
-							  |RCC_OSCILLATORTYPE_LSE;
-	RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-	RCC_OscInitStruct.LSEState = RCC_LSE_ON;
-	RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-	RCC_OscInitStruct.HSICalibrationValue = 16;
-	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-	RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
-	RCC_OscInitStruct.PLL.PREDIV = RCC_PREDIV_DIV1;
-	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-	{
-		//_Error_Handler(__FILE__, __LINE__);
-	}
-
-	// Initializes the CPU, AHB and APB busses clocks
-	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-							  |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
-
-	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
-	{
-		//_Error_Handler(__FILE__, __LINE__);
-	}
-
-	PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USB|RCC_PERIPHCLK_USART2
-							  |RCC_PERIPHCLK_I2C2|RCC_PERIPHCLK_I2C3
-							  |RCC_PERIPHCLK_RTC|RCC_PERIPHCLK_ADC12;
-	PeriphClkInit.Usart2ClockSelection = RCC_USART2CLKSOURCE_PCLK1;
-	PeriphClkInit.Adc12ClockSelection = RCC_ADC12PLLCLK_DIV1;
-	PeriphClkInit.I2c2ClockSelection = RCC_I2C2CLKSOURCE_HSI;
-	PeriphClkInit.I2c3ClockSelection = RCC_I2C3CLKSOURCE_HSI;
-	PeriphClkInit.RTCClockSelection = RCC_RTCCLKSOURCE_LSE;
-	PeriphClkInit.USBClockSelection = RCC_USBCLKSOURCE_PLL_DIV1_5;
-	if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
-	{
-		//_Error_Handler(__FILE__, __LINE__);
-	}
-
-	// Configure the Systick interrupt time
-	HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
-
-	// Configure the Systick
-	HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
-
-	// SysTick_IRQn interrupt configuration
-	HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+	// Init UART
+	uart_init(CONSOLE_UART, 115200, NULL, NULL);
+	uart_init(GPS_UART, 9600, gps_uart_rx_interrupt, NULL);
+	uart_init(COMMAND_UART, 115200, command_uart_rx_interrupt, NULL);
 }
