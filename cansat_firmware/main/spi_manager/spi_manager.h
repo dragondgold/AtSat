2a6d6ca8b1081d9dd1a898ddd3d81dee40a20b2b
Andres <torti.max@gmail.com>
2019-01-31 23:50:34 -0300

    Método sincrónico thread-safe para enviar bytes agregado a spi_manager

--- cansat_firmware/main/spi_manager/spi_manager.h
+++ cansat_firmware/main/spi_manager/spi_manager.h
@@ -3,8 +3,35 @@
 
 #include <stdbool.h>
 #include "esp_system.h"
+#include "freertos/FreeRTOS.h"
+#include "driver/spi_common.h"
+#include "driver/spi_master.h"
 
 esp_err_t spi_manager_init(void);
+
+/**
+ * @brief Acquire permission to use the SPI port
+ * 
+ * @param timeout max time to wait for the port
+ * @return esp_err_t ESP_OK if the port was acquired.
+ */
+esp_err_t spi_manager_acquire(TickType_t timeout);
+/**
+ * @brief Release the SPI port so others can use it
+ * 
+ * @return esp_err_t ESP_OK if the port was released.
+ */
+esp_err_t spi_manager_release(void);
+
+/**
+ * @brief Transmit the requested spi_transaction_t to the SPI bus. This waits for
+ *  the SPI data to be send synchronously, this method is not async.
+ * 
+ * @param trans transaction to send and used to receive the data back
+ * @param timeout max time to wait for the SPI bus to be available
+ * @return esp_err_t ESP_OK if transaction was successfull
+ */
+esp_err_t spi_manager_device_transmit(spi_transaction_t* trans, TickType_t timeout);
 
 void spi_manager_enable_buffer(void);
 void spi_manager_disable_buffer(void);
