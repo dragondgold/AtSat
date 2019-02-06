00e153ca8fb3753779499815286956fa4a20ffda
Andres <torti.max@gmail.com>
2019-02-05 21:04:48 -0300

    Corrección de valores de duty para los servos

--- cansat_firmware/main/servo_manager/servo_manager.h
+++ cansat_firmware/main/servo_manager/servo_manager.h
@@ -7,8 +7,8 @@
 #define SERVO_MANAGER_PARACHUTE_CHANNEL     LEDC_CHANNEL_0
 #define SERVO_MANAGER_BALLOON_CHANNEL       LEDC_CHANNEL_1
 
-#define SERVO_MANAGER_CLOSE_SERVO_DUTY      5.0       // 5 % of 50 Hz
-#define SERVO_MANAGER_OPEN_SERVO_DUTY       10.0      // 10 % of 50 Hz
+#define SERVO_MANAGER_CLOSE_SERVO_DUTY      2.0       // 5 % of 50 Hz
+#define SERVO_MANAGER_OPEN_SERVO_DUTY       15.0      // 10 % of 50 Hz
 
 esp_err_t servo_manager_init(void);
 
