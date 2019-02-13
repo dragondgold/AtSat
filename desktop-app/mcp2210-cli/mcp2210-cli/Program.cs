a3846cf58429a9816c54c55c2468b54cbc208b54
Andres <torti.max@gmail.com>
2019-02-13 16:56:07 -0300

    Cambiado CLI provisto por Microchip por uno propio debido a bugs que presentaba

---
+++ desktop-app/mcp2210-cli/mcp2210-cli/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mcp2210_cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
