606349cce0835feec1b09160fcf08cec91bff4a9
Andres <torti.max@gmail.com>
2019-02-15 01:08:05 -0300

    Soporte experimental para manejo de GPIO desde mcp2210-cli

--- desktop-app/mcp2210-cli/mcp2210-cli/Program.cs
+++ desktop-app/mcp2210-cli/mcp2210-cli/Program.cs
@@ -18,29 +18,23 @@ namespace mcp2210_cli
 
         static int Main(string[] args)
         {
-            // Measure execution time
-            var sw = Stopwatch.StartNew();
+            // Default SPI parameters
+            bool shouldShowHelp = false;
+            uint deviceIndex = 0;
+            uint pbaudRate2 = 1000000;
+            uint pidleCsVal2 = 0xfff;       // Idle value for CS
+            uint pactiveCsVal2 = 0x000;     // Active value for CS
+            uint pcsToDataDly2 = 0;         // CS to data delay
+            uint pdataToCsDly2 = 0;         // Last data byte to CS delay
+            uint pdataToDataDly2 = 0;       // Delay between bytes
+            uint ptxferSize2 = 4;           // Bytes to Transfer per SPI Transaction
+            byte pspiMd2 = 0;               // SPI Mode
+            uint csIndex = 0;
+            String gpio = null;
+            String data = null;
 
-            int devCount = MCP2210.M_Mcp2210_GetConnectedDevCount(DEFAULT_VID, DEFAULT_PID);
-            if(DEBUG) Console.WriteLine(devCount + " devices found");
-
-            if (devCount > 0)
-            {
-                bool shouldShowHelp = false;
-                uint deviceIndex = 0;
-                uint pbaudRate2 = 1000000;
-                uint pidleCsVal2 = 0xfff;       // Idle value for CS
-                uint pactiveCsVal2 = 0x000;     // Active value for CS
-                uint pcsToDataDly2 = 0;         // CS to data delay
-                uint pdataToCsDly2 = 0;         // Last data byte to CS delay
-                uint pdataToDataDly2 = 0;       // Delay between bytes
-                uint ptxferSize2 = 4;           // Bytes to Transfer per SPI Transaction
-                byte pspiMd2 = 0;               // SPI Mode
-                uint csIndex = 0;
-                String data = null;
-
-                // Set the command line parser
-                var options = new OptionSet {
+            // Set the command line parser
+            var options = new OptionSet {
                     { "i|index=", $"index of the MCP2210 device (defaults to {deviceIndex})", (uint i) => deviceIndex = i },
                     { "b|baud=", $"baud rate in MHz (defaults to {pbaudRate2})", (uint b) => pbaudRate2 = b },
                     { "csToDataDly=", $"delay between CS assertion and data send in units of 100 us (defaults to {pcsToDataDly2})", (uint d) => pcsToDataDly2 = d },
@@ -48,36 +42,42 @@ namespace mcp2210_cli
                     { "dataToDataDly=", $"delay between bytes in units of 100 us (defaults to {pdataToDataDly2})", (uint d) => pdataToDataDly2 = d },
                     { "m|mode=", $"SPI mode (defaults to {pspiMd2})", (byte m) => pspiMd2 = m },
                     { "cs=", $"CS index (defaults to {csIndex})", (uint i) => csIndex = i },
-                    {"d|data=", "Array of bytes to send in hex format separated by commas", s => data = s },
+                    { "d|data=", "Array of bytes to send in hex format separated by commas", s => data = s },
+                    { "gpioW=[number][1|0]", "Write 1 or 0 to the specified GPIO (example: gpioW=21, write 1 to GPIO 2)", s => gpio = s },
                     { "h|help", "show this message and exit", h => shouldShowHelp = h != null },
                 };
-                try
-                {
-                    // Parse the command line
-                    options.Parse(args);
+            try
+            {
+                // Parse the command line
+                options.Parse(args);
 
-                    // Show help menu
-                    if(shouldShowHelp)
-                    {
-                        options.WriteOptionDescriptions(Console.Out);
-                        return 0;
-                    }
-
-                    // We always need data
-                    if(data == null)
-                    {
-                        return 0;
-                    }
-                }
-                catch (OptionException e)
+                // Show help menu
+                if (shouldShowHelp)
                 {
-                    // output some error message
-                    Console.Write("greet: ");
-                    Console.WriteLine(e.Message);
-                    Console.WriteLine("Try `greet --help' for more information.");
+                    options.WriteOptionDescriptions(Console.Out);
+                    return 0;
                 }
 
+                // We always need data
+                if (data == null)
+                {
+                    return 0;
+                }
+            }
+            catch (OptionException e)
+            {
+                // output some error message
+                Console.Write("greet: ");
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Try `greet --help' for more information.");
+            }
 
+            // Search connected MCP2210 devices
+            int devCount = MCP2210.M_Mcp2210_GetConnectedDevCount(DEFAULT_VID, DEFAULT_PID);
+            if(DEBUG) Console.WriteLine(devCount + " devices found");
+
+            if (devCount > 0)
+            {
                 // Connect to the MCP2210
                 StringBuilder path = new StringBuilder();
                 IntPtr deviceHandle = new IntPtr();
@@ -90,6 +90,26 @@ namespace mcp2210_cli
                 {
                     Console.WriteLine("Failed to open connection");
                     return -1;
+                }
+
+                // Check if any GPIO must be written
+                if(gpio != null && gpio.Length == 2)
+                {
+                    uint gpioNumber = uint.Parse(gpio[0].ToString());
+                    uint value = uint.Parse(gpio[1].ToString());
+                    byte[] pins = new byte[MCP2210.M_MCP2210_GPIO_NR];
+
+                    uint gpioValue = (uint)(1 << (int)gpioNumber);
+
+                    // Set everything as GPIO
+                    for(int n = 0; n < pins.Length; ++n)
+                    {
+                        pins[n] = (byte)MCP2210.M_MCP2210_PIN_DES_GPIO;
+                    }
+
+                    // Set GPIO
+                    MCP2210.M_Mcp2210_SetGpioConfig(deviceHandle, (byte)MCP2210.M_MCP2210_VM_CONFIG, pins, gpioValue, 0, (byte)MCP2210.M_MCP2210_REMOTE_WAKEUP_DISABLED,
+                        (byte)MCP2210.M_MCP2210_INT_MD_CNT_NONE, (byte)MCP2210.M_MCP2210_SPI_BUS_RELEASE_DISABLED);
                 }
 
                 // Convert the data string to bytes to send
@@ -135,9 +155,6 @@ namespace mcp2210_cli
                 Console.WriteLine(rxString);
 
                 MCP2210.M_Mcp2210_Close(deviceHandle);
-                sw.Stop();
-
-                Console.WriteLine("Took: " + sw.ElapsedMilliseconds + " ms");
             }
 
             return 0;
