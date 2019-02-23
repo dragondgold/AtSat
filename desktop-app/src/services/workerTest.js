bb16cc992418922356903443472d6f4d12fb84c8
Andres <torti.max@gmail.com>
2019-02-23 17:14:09 -0300

    Nuevo workerTest.js

--- desktop-app/src/services/workerTest.js
+++ desktop-app/src/services/workerTest.js
@@ -1,15 +1,76 @@
-const path = require('path');
 const cc1101 = require('./cc1101.js');
-const protocol = require('./protocol.js');
 
-let usb;
+cc1101.cc1101_init("COM3").then(function(r)
+{
+    console.log(r);
+    console.time("servo");
+    cc1101.cc1101_send_data([0x7e, 0,2, 2,1, 252]).then(function()
+    {
+      setInterval(function()
+      {
+        cc1101.cc1101_bytes_in_rx_fifo().then(function(bytes)
+        {
+          console.log("Bytes: " + bytes);
+        });
+      }, 100)
 
-let pathToCLI = path.resolve('./../../build/MCP2210/mcp2210-cli.exe');
+      console.timeEnd("servo");
+      console.log("Sent!");
+    })
+    .catch(function(err)
+    {
+      console.log("Fail sent: " + err);
+    });
+})
+.catch(function(e)
+{
+    console.error(e);
+});
 
-let init = false
-while(!cc1101.cc1101_init(pathToCLI)){
+/*
+var SerialPort = require("serialport");
+var port = new SerialPort("COM3", {
+      baudRate: 115200
+});
+const Readline = require('@serialport/parser-readline')
+const parser = new Readline()
 
+var buffer = new Buffer(5);
+buffer[0] = 0x7E;
+buffer[1] = 0x02;
+buffer[2] = 0x01;
+buffer[3] = 0x37;
+buffer[4] = 0xC7;
+
+function toHexString(byteArray) {
+  return Array.from(byteArray, function(byte) {
+    return ('0' + (byte & 0xFF).toString(16)).slice(-2);
+  }).join(' ')
 }
+
+port.on("open", function () {
+	console.log('Port opened!');
+	
+	setInterval(function()
+	{
+		console.time("send");
+		port.write(buffer);
+	}, 100);
+});
+
+// open errors will be emitted as an error event 
+port.on('error', function(err) {
+  console.log('Error -> ', err.message);
+})
+
+port.on('data', function(data)
+{
+	console.timeEnd("send");
+  console.log("Received: " + toHexString(data));
+});*/
+
+/*
+cc1101.cc1101_init();
 
 console.log("Init: " + true);
 
@@ -53,4 +114,4 @@ setInterval(function()
   //cc1101.cc1101_send_data([0x7E, 0x00, 0x07, 5,3,1,2,4,6,7, 227]);
   cc1101.cc1101_send_data([0x7E, 0x00, 0x01, 1,254 ]);
 
-}, 1000);
+}, 1000);*/
