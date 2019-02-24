8a9614cd75739ba5a65582e317417b23df04ca78
Andres <torti.max@gmail.com>
2019-02-24 02:17:34 -0300

    Dutys independientes para servos de globo y paracaídas

--- cansat_firmware/main/servo_manager/servo_manager.h
+++ cansat_firmware/main/servo_manager/servo_manager.h
@@ -8,8 +8,10 @@
 #define SERVO_MANAGER_PARACHUTE_CHANNEL     LEDC_CHANNEL_0
 #define SERVO_MANAGER_BALLOON_CHANNEL       LEDC_CHANNEL_1
 
-#define SERVO_MANAGER_CLOSE_SERVO_DUTY      2.0       // 5 % of 50 Hz
-#define SERVO_MANAGER_OPEN_SERVO_DUTY       15.0      // 10 % of 50 Hz
+#define SERVO_MANAGER_PARACHUTE_CLOSE_SERVO_DUTY        2.0       // 2 % of 50 Hz
+#define SERVO_MANAGER_PARACHUTE_OPEN_SERVO_DUTY         4.0       // 5 % of 50 Hz
+#define SERVO_MANAGER_BALLOON_CLOSE_SERVO_DUTY          2.0
+#define SERVO_MANAGER_BALLOON_OPEN_SERVO_DUTY           4.0
 
 esp_err_t servo_manager_init(void);
 
